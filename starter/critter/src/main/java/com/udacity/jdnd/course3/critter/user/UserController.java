8f156cbaf3c8599ab4766ff661b84cdb5208ddba
DemonDaddy22 <guptarohan369@gmail.com>
2020-11-08 16:17:20 +0530

    add schedule controller methods

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
@@ -1,8 +1,15 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.service.CustomerService;
+import com.udacity.jdnd.course3.critter.service.EmployeeService;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -16,39 +23,118 @@ import java.util.Set;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private PetService petService;
+
     @PostMapping("/customer")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
-        throw new UnsupportedOperationException();
+        List<Long> petIds = customerDTO.getPetIds();
+        List<Pet> pets = new ArrayList<>();
+
+        if (petIds != null) {
+            petIds.forEach(petId -> pets.add(petService.findPet(petId)));
+        }
+
+        Customer customer = convertCustomerDTOToCustomer(customerDTO);
+        customer.setPets(pets);
+        return convertCustomerToCustomerDTO(customerService.save(customer));
+
     }
 
     @GetMapping("/customer")
     public List<CustomerDTO> getAllCustomers(){
-        throw new UnsupportedOperationException();
+        List<Customer> customers = customerService.findAllCustomers();
+        List<CustomerDTO> customerDTOs = new ArrayList<>();
+
+        customers.forEach(customer -> customerDTOs.add(convertCustomerToCustomerDTO(customer)));
+
+        return customerDTOs;
     }
 
     @GetMapping("/customer/pet/{petId}")
     public CustomerDTO getOwnerByPet(@PathVariable long petId){
-        throw new UnsupportedOperationException();
+        Pet pet = petService.findPet(petId);
+        Customer customer = pet.getCustomer();
+
+        if (customer != null) return convertCustomerToCustomerDTO(customer);
+
+        return null;
     }
 
     @PostMapping("/employee")
     public EmployeeDTO saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        throw new UnsupportedOperationException();
+        Employee employee = convertEmployeeDTOToEmployee(employeeDTO);
+        return convertEmployeeToEmployeeDTO(employeeService.save(employee));
     }
 
-    @PostMapping("/employee/{employeeId}")
+    @GetMapping("/employee/{employeeId}")
     public EmployeeDTO getEmployee(@PathVariable long employeeId) {
-        throw new UnsupportedOperationException();
+        Employee employee = employeeService.findEmployee(employeeId);
+
+        if (employee != null) return convertEmployeeToEmployeeDTO(employee);
+
+        return null;
     }
 
     @PutMapping("/employee/{employeeId}")
     public void setAvailability(@RequestBody Set<DayOfWeek> daysAvailable, @PathVariable long employeeId) {
-        throw new UnsupportedOperationException();
+        employeeService.setDaysAvailable(daysAvailable, employeeId);
     }
 
     @GetMapping("/employee/availability")
     public List<EmployeeDTO> findEmployeesForService(@RequestBody EmployeeRequestDTO employeeDTO) {
-        throw new UnsupportedOperationException();
+        List<Employee> employees = employeeService.findEmployeesAvailable(employeeDTO.getDate(), employeeDTO.getSkills());
+        List<EmployeeDTO> employeeDTOs = new ArrayList<>();
+
+        employees.forEach(employee -> employeeDTOs.add(convertEmployeeToEmployeeDTO(employee)));
+
+        return employeeDTOs;
+    }
+
+    private CustomerDTO convertCustomerToCustomerDTO(Customer customer){
+        CustomerDTO customerDTO = new CustomerDTO();
+        BeanUtils.copyProperties(customer, customerDTO);
+        List<Pet> pets = customer.getPets();
+
+        if(pets != null) {
+            List<Long> petIds = new ArrayList<>();
+            pets.forEach(pet -> petIds.add(pet.getId()));
+            customerDTO.setPetIds(petIds);
+        }
+
+        return customerDTO;
+    }
+
+    private Customer convertCustomerDTOToCustomer(CustomerDTO customerDTO){
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerDTO, customer);
+        List<Long> petIds = customerDTO.getPetIds();
+
+        if(petIds != null){
+            List<Pet> pets = new ArrayList<Pet>();
+            petIds.forEach(petId -> pets.add(petService.findPet(petId)));
+            customer.setPets(pets);
+        }
+
+        return customer;
+    }
+
+    private EmployeeDTO convertEmployeeToEmployeeDTO(Employee employee){
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+        BeanUtils.copyProperties(employee, employeeDTO);
+        return employeeDTO;
+    }
+
+    private Employee convertEmployeeDTOToEmployee(EmployeeDTO employeeDTO){
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);
+        return employee;
     }
 
 }
